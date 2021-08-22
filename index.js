29e583bb15aeb730f5a6fcfdb4717b60c72d808c
Michael Stephens <mdstephens26@yahoo.com>
2021-08-21 21:27:26 -0400

    commit tree 504113c5891eba1f72477d1d5c9a3116981a210e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(nameArray, eventType) {
+    let thankYouCards = []
+    for(let i = 0; i < nameArray.length; i++){
+        thankYouCards.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventType} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(number) {
+    while (number >0 ) {
+        console.log(number--)
+    }
+    console.log(number)
+}
